e8c14dd35fc0ebc35a8531044ae331e9dbd8e675
Huy Trinh Ba <huytb@tmh-techlab.vn>
2018-09-11 09:53:51 +0700

    draw from canvas to img

--- coco-collage.js
+++ coco-collage.js
@@ -1,51 +1,26 @@
 $(document).ready(function(){
-	$(":file").css("opacity", 0);
-	function readURL(input) {
-		if (input.files && input.files[0]) {
-			var reader = new FileReader();
+    $(":file").css("opacity", 0);
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-		   	reader.onload = function(e) {
-		   		input.nextElementSibling.setAttribute("src", e.target.result);
-			}
+            reader.onload = function(e) {
+                input.nextElementSibling.setAttribute("src", e.target.result);
+            }
 
-		reader.readAsDataURL(input.files[0]);
-		}
-	}
-	$('.x').click(function(event) {
-	    if (!$(event.target).is('.collage')) {
-		    $(this).find(".collage").trigger('click');
-		}
-	});
-    $('#a').click(function(event) {
-        if (!$(event.target).is('#img1')) {
-            $(this).find("#img1").trigger('click');
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $('.x').click(function(event) {
+        if (!$(event.target).is('.collage')) {
+            $(this).find(".collage").trigger('click');
         }
     });
-	$('#b').click(function(event) {
-	    if (!$(event.target).is('#img2')) {
-	        $(this).find("#img2").trigger('click');
-	    }
-	});
-	$('#c').click(function(event) {
-	    if (!$(event.target).is('#img3')) {
-		    $(this).find("#img3").trigger('click');
-	   }
-	});
-    $('#d').click(function(event) {
-        if (!$(event.target).is('#img4')) {
-            $(this).find("#img4").trigger('click');
-        }
+    
+    $(":file").change(function() {
+        readURL(this);
+        $(this).next().css('display', 'block');
     });
-    $('#e').click(function(event) {
-        if (!$(event.target).is('#img5')) {
-            $(this).find("#img5").trigger('click');
-        }
-    });
-
-	$(":file").change(function() {
-  		readURL(this);
-  		$(this).next().css('display', 'block');
-	});
 
     var element = $("#img"); // global variable
     var getCanvas; // global variable
@@ -54,10 +29,15 @@ $(document).ready(function(){
         $('.collage').remove();
         html2canvas(element, {
             onrendered: function (canvas) {
+                canvas.id = "can";
                 $("#previewImage").append(canvas);
                 getCanvas = canvas;
+                var image = new Image();
+                image.src = document.getElementById('can').toDataURL();
+                $('body').append(image);
+                canvas.setAttribute("style","display: none");
             }
         });
+        
     });
-
 });
